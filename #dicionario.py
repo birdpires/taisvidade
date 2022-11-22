e36b300b97f8eb373b8bbb8f7b53d8b9a2f34be0
Rodrygo <113386711+birdpires@users.noreply.github.com>
2022-11-22 17:57:40 -0300

    Add files via upload

---
+++ #dicionario.py
@@ -0,0 +1,17 @@
+#dicionario
+
+taisvidade = {
+    "C#": {"programa": "C#", "salario": "6.450,00"},
+    "Java": {"programa": "Java", "salario": "7.400,00"},
+    "Android": {"programa": "Android", "salario": "7.000,00"},
+    "Ios": {"programa": "Ios", "salario": "10.500,00"},
+    "Swift": {"programa": "Swift", "salario": "9.600,00"},
+}
+
+taisvidade["C#"]["salario"] = "7.450,00"
+taisvidade["Java"]["salario"] = "8.400,00"
+taisvidade["Android"]["salario"] = "8.000,00"
+taisvidade["Ios"]["salario"] = "11.500,00"
+taisvidade["Swift"]["salario"] = "10.600,00"
+
+print(taisvidade["Android"])
